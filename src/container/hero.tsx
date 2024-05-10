3db074fbb44c06b963fe1851a1c79cd7746f0265
heispapijay <heispapijay@gmail.com>
2024-05-11 02:09:00 +0300

    Update HTML doctype declaration to use uppercase

--- src/container/hero.tsx
+++ src/container/hero.tsx
@@ -1,18 +1,14 @@
 import Button from '../components/Button'
 import './hero.scss'
-// import card from '../assets/image.png'
-// import pos from '../assets/image1.png'
 
 const Hero = () => {
   return (
     <div className="wrapper">
         <div className='image-cards'>
-            {/* <img src={pos} alt="pos" />
-            <img src={card} alt="card" /> */}
         </div>
         <div className="content">
             <h1>Craft charming, face-to-face payment experiences</h1>
-            <p>Greep POS: DePIN payments Network for Merchants: Empowers businesses to craft personalized, seamless, and in-person payment experiences that foster customer loyalty and drive repeat business</p>
+            <p>Greep Mobile POS: DePIN payments Network for Merchants: Empowers businesses to craft personalized, seamless, and in-store payment experiences that foster customer loyalty and drive repeat business</p>
             <div className='cta'>
                 <Button text="Get Started" />
                 <button className='btn'>Learn more</button>
