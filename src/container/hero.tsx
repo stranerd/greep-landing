97ba901398d89568fb928150ad6a25c4486cdd68
heispapijay <heispapijay@gmail.com>
2024-05-11 03:09:02 +0300

    chore: Remove commented out code and add animation to hero title

--- src/container/hero.tsx
+++ src/container/hero.tsx
@@ -1,13 +1,18 @@
 import Button from '../components/Button'
 import './hero.scss'
+import greepvid from "../assets/greepvd.mp4"
 
 const Hero = () => {
   return (
     <div className="wrapper">
         <div className='image-cards'>
+        <video loop autoPlay muted>
+        <source src={greepvid} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
         </div>
         <div className="content">
-            <h1>Craft charming, face-to-face payment experiences</h1>
+            <h1><span className="animate">Craft charming, face-to-face payment experiences</span></h1>
             <p>Greep Mobile POS: DePIN payments Network for Merchants: Empowers businesses to craft personalized, seamless, and in-store payment experiences that foster customer loyalty and drive repeat business</p>
             <div className='cta'>
                 <Button text="Get Started" />
