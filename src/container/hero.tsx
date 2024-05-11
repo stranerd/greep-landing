34490651410b5cd7517029b6665bd9f2d12048a9
heispapijay <heispapijay@gmail.com>
2024-05-11 10:53:55 +0300

    chore: Update hero container layout and styles

--- src/container/hero.tsx
+++ src/container/hero.tsx
@@ -1,29 +1,33 @@
-import Button from '../components/Button'
-import './hero.scss'
-import greepvid from "../assets/greepvd.mp4"
+import Button from "../components/Button";
+import "./hero.scss";
+import greepvid from "../assets/greepvd.mp4";
 
 const Hero = () => {
   return (
     <div className="wrapper">
-        <div className='image-cards'>
+      <div className="image-cards">
         <video loop autoPlay muted>
-        <source src={greepvid} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+          <source src={greepvid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="content">
+        <span className="powered">Powered by Stellar Network</span>
+        <h1>
+          <span className="animate">Decentralized Payments Infrastructure</span>
+        </h1>
+        <p>
+          Process Payment Real time leverages the stellar Network in a
+          versatile retail setting <br /> Earn royalty Reward in real Time
+        </p>
+        <div className="cta">
+          <Button text="Pre-order" />
+          <button className="btn">Learn more</button>
         </div>
-        <div className="content">
-            <h1><span className="animate">Craft charming, face-to-face payment experiences</span></h1>
-            <p>Greep Mobile POS: DePIN payments Network for Merchants: Empowers businesses to craft personalized, seamless, and in-store payment experiences that foster customer loyalty and drive repeat business</p>
-            <div className='cta'>
-                <Button text="Get Started" />
-                <button className='btn'>Learn more</button>
-            </div>
-            <div>
-            
-            </div>
-        </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
